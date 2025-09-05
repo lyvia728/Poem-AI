7cdaba4e4094d1aa1a39a102abbff75a35278e05
Welile Nkomo <welilelyvia@gmail.com>
2025-09-05 20:11:05 +0200

    Fixed the axois

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function displayPoem(response) {
-
-  new Typewriter("#poem", {
+    console.log("Poem generated");
+   new Typewriter("#poem", {
         strings: response.data.answer,
         autoStart: true,
         delay: 1,
@@ -8,15 +8,7 @@ function displayPoem(response) {
 
     });
 
-    
-
 }
-
-
-
-
-
-
 
 function generatePoem(event) {
     event.preventDefault();
@@ -24,14 +16,14 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "eba2o4c7f9e18e3f1t9e2154f4dd3105";
     let prompt = `Generate a  poem about ${instructionsInput.value}`;
-    let context = "Write a short poem in a creative way";
+    let context = "Write a short poem in a creative way of five lines, make a line break using <br/>";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
     console.log("Generating poem...");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
 
-    axois.get(apiUrl).then(displayPoem);
+    axios.get(apiUrl).then(displayPoem);
 
 
 
